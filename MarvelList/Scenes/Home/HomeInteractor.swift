46b09e044238a5eab7af80f703feb35e451b8ec1
Alexandre Abreu <alexandre.oliveira.abreu@everis.com>
2020-10-06 17:15:32 -0300

    git ignore

---
+++ MarvelList/Scenes/Home/HomeInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  HomeInteractor.swift
+//  MarvelList
+//
+//  Created by MacDev on 06/10/20.
+//  Copyright © 2020 Alexandre Abreu. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelList/Scenes/Home/HomePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  HomePresenter.swift
+//  MarvelList
+//
+//  Created by MacDev on 06/10/20.
+//  Copyright © 2020 Alexandre Abreu. All rights reserved.
+//
+
+import Foundation
